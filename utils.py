6d105cc341cd53a946cc181c6a1d7f136ecf0588
john <jogp10@gmail.com>
2024-03-05 22:21:50 +0000

    Graph creation for SNA

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 def initial_obs(df):
-    display(df.head(3))
+    display(df.head(10))
     print(f"\n\033[1mAttributes:\033[0m {list(df.columns)}")
     print(f"\033[1mEntries:\033[0m {df.shape[0]}")
     print(f"\033[1mAttribute Count:\033[0m {df.shape[1]}")
